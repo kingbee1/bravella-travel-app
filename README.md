e6060e7d0f3229a25fbcef04cd92bacf052ee860
Abiodun <abiodunhodonu@gmail.com>
2023-07-08 23:11:58 +0100

    setup header component

---
+++ README.md
@@ -0,0 +1,3 @@
+## Bravella Travel Companion App
+
+Dependecies I used npm install @material-ui/core @material-ui/icons @material-ui/lab @react-google-maps/api axios google-map-react 
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+//CBL is an mui component that normalises styles. it fixes padding,magging and background colours.
+import { CssBaseline, Grid } from "@material-ui/core";
+
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
 
-      <div>
-
-      </div>
-
-  )
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
-export default App
+export default App;
